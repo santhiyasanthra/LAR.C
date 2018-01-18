dcb317905344f84c7dac75e54d2dfa2fdf8b424a
santhiyasanthra <31505577+santhiyasanthra@users.noreply.github.com>
2018-01-18 21:11:42 +0530

    Create LLL.C

---
+++ LLL.C
@@ -0,0 +1,21 @@
+ 
+int main()
+{ 
+    int a,b,c;
+    printf("Enter three numbers:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b && a>c)        
+        {
+        printf(" a is the largest",a);
+        }
+    else if(b>a && b>c)
+    {
+    printf("b is the largest",b);
+    }
+    else
+    {
+    printf("c is the Largest number,c);
+    }
+ 
+    return 0;
+}
